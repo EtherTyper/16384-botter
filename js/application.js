fdb42b0c4081d9cfc3952187be6078300ebcf9b9
ELI JOSEPH BRADLEY <EtherTyper@users.noreply.github.com>
2016-12-30 19:07:32 -0600

    Update application.js

--- js/application.js
+++ js/application.js
@@ -1,12 +1,10 @@
-
-
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
   window.game = new GameManager(8, KeyboardInputManager, HTMLActuator, LocalScoreManager);
 });
 
 for (var i=0;; i = (i + 1) % 4){
-  window.setTimeout(function () {
-    window.game.move(i);
-  }, 500);
+//   window.setTimeout(function () {
+//     window.game.move(i);
+//   }, 500);
 }
