e7de25d445d29951559c83824bd776bc0d63f615
ELI JOSEPH BRADLEY <EtherTyper@users.noreply.github.com>
2016-12-30 19:03:11 -0600

    Add botting

--- js/application.js
+++ js/application.js
@@ -1,4 +1,10 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-  new GameManager(8, KeyboardInputManager, HTMLActuator, LocalScoreManager);
+  var game = new GameManager(8, KeyboardInputManager, HTMLActuator, LocalScoreManager);
+  
+  for (var i=0;; i = (i + 1) % 4){
+    window.setTimeout(function () {
+      game.move(i);
+    }, 500);
+  }
 });
